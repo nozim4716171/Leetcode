6501c983627ccda9ece7f045473392d53c485f44
Nozimjon Hamdamov <77096481+nozim4716171@users.noreply.github.com>
2023-03-28 01:30:28 +0500

    Create 50.Pow(x, n).py

---
+++ 50.Pow(x, n).py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        result=x**n
+        return result
